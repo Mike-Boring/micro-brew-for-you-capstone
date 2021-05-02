d37a6c29e25b420723fb3a9e36a9eacf8ed3bd56
Mike Boring <shekinah999@msn.com>
2021-05-02 16:49:20 -0600

    Setting up for deployment

--- microbrewforyou_server/settings.py
+++ microbrewforyou_server/settings.py
@@ -16,7 +16,7 @@ env = environ.Env(
 # reading .env file
 environ.Env.read_env()
 
-
+# ok
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
